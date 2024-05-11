dca8a5ae0a4d33b3fa42ce57b0bfc35d958260bd
ManueldeMacedo <manuel_demacedo@hotmail.com>
2024-05-11 15:12:17 -0300

    Se agrego RouterProvider y NotFound component

--- tpi-lab3/src/App.jsx
+++ tpi-lab3/src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import Layout from "./components/layout/Layout";
+import NotFound from "./components/notFound/NotFound";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+          <Layout></Layout>
+      ),
+    },
+    {
+      path: "*",
+      element: <NotFound></NotFound>,
+    },
+  ]);
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      {<RouterProvider router={router} />}
+    </div>
+  );
 }
 
 export default App;
