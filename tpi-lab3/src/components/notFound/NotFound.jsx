f266dad056b5ba02583ab304f8f203716e929bba
Fede Alvarado <federico.alvarado96@gmail.com>
2024-06-20 18:15:36 -0300

    Update NotFound

--- tpi-lab3/src/components/notFound/NotFound.jsx
+++ tpi-lab3/src/components/notFound/NotFound.jsx
@@ -1,5 +1,7 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { TbError404Off } from "react-icons/tb";
+
 
 const NotFound = () => {
   const navigate = useNavigate();
@@ -10,7 +12,12 @@ const NotFound = () => {
 
   return (
     <>
-      <div>No hay nada para este path</div>
+    <TbError404Off fontSize={"500px"} fluid/>
+
+      <div>
+        <h1>Not found</h1>
+        </div>
+
       <Button onClick={handleClick}>Volver al inicio</Button>
     </>
   );
